de23ba941614b97fae84f71e5684b5a236880808
JuanDavid1925 <juan.cabrera.lopez@correounivalle.edu.co>
2023-10-04 21:41:39 -0500

    Modal assistant

---
+++ frontend/src/components/diagram/ModalAssistant.jsx
@@ -0,0 +1,104 @@
+import React, { useState } from 'react';
+import * as AssistantService from "../../service/AssistantService"
+
+
+function ModalAssistant(props) {
+
+  const [activities, setActivities] = useState([]);
+  const [description, setDescription] = useState('');
+
+  const addActivity = () => {
+    setActivities([...activities, ["", ""]]);
+  }
+
+  const deleteActivity = (position) => {
+    setActivities([...activities.filter((_, index) => index !== position)]);
+  };
+
+  const handleChangeActivities = (e, index) => {
+    activities[index][0] = e.target.value;
+    setActivities([...activities]);
+  };
+
+  const handleChangeResponsibles = (e, index) => {
+    activities[index][1] = e.target.value;
+    setActivities([...activities]);
+  };
+
+  const handleChangeDescription = (e) => {
+    setDescription(e.target.value);
+  };
+
+  const handleSubmit = async (evt) => {
+    evt.preventDefault();
+    console.log(activities);
+    console.log(description);
+    const response = await AssistantService.autocomplete(description, activities);
+  };
+
+  return (
+    <div className="modal fade" id="modalDiagram" aria-labelledby="tittleModalDiagram" aria-hidden="true" ref={props.refModalAssistant}>
+      <div className="modal-dialog modal-dialog-centered">
+        <div className="modal-content bg-two border-0">
+          <div className="modal-header bg-one">
+            <h5 className="modal-title text-white" id="tittleModalDiagram"> How can I help you?</h5>
+            <button type="button" className="btn-one px-1" data-bs-dismiss="modal" aria-label="Close">
+              <i className="bi bi-x-lg"></i>
+            </button>
+          </div>
+          <form onSubmit={handleSubmit}>
+            <div className="modal-body">
+              <div>
+                <label className="form-label">Description:</label>
+                <textarea className="form-control" required rows="5" onChange={handleChangeDescription} name='description' ></textarea>
+              </div>
+              <hr className="hr hr-blurry" />  {/*Divider*/}
+              <label className="form-label">More Details: </label>
+              <div className="modal-footer border-0">
+                <button type="button" id='add' className="btn-one shadow-lg py-1"
+                  onClick={() => addActivity()}
+                > <i className="bi bi-plus-circle"></i> Add</button>
+              </div>
+              <div>
+                {
+                  activities.map((activity, index) => (
+                    <div key={index + 1}>
+                      <div className='row' >
+                        <div className='col-10' >
+                          <div className="mb-2 mt-2">
+                            <label className="form-label">Activity: {index + 1}</label>
+                            <input className="form-control" onChange={(e) => handleChangeActivities(e, index)} name='activity' />
+                          </div>
+                        </div>
+                        <div className="col" >
+                          <button type="button" className="btn btn-secondary shadow-lg py-1 mt-2"
+                            onClick={() => { deleteActivity(index) }}
+                          > <i className="bi bi-trash"></i>
+                          </button>
+                        </div>
+                      </div>
+                      <div className='row'>
+                        <div className='col-10'>
+                          <div className="mb-3">
+                            <label className="form-label">Responsible: </label>
+                            <input className="form-control" onChange={(e) => handleChangeResponsibles(e, index)} name='responsible' />
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  ))
+                }
+              </div>
+              <div className="modal-footer border-0">
+                <button type="button" className="btn-two shadow-lg py-1" data-bs-dismiss="modal">Close</button>
+                <button type="submit" className="btn-one shadow-lg py-1" >Create</button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ModalAssistant;
